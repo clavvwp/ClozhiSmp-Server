37c2dca29d5471b7034af680ce9bc76db37628a6
Clav <clavvwp@gmail.com>
2026-05-27 12:19:34 +0700

    Hard test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,15 @@
-'use client';
-
-import Navbar from '@/components/ui/Navbar';
-import HeroSection from '@/components/sections/HeroSection';
-import FeaturesSection from '@/components/sections/FeaturesSection';
-import ScreenshotSection from '@/components/sections/ScreenshotSection';
-import ServerStatusSection from '@/components/sections/ServerStatusSection';
-import CommunitySection from '@/components/sections/CommunitySection';
-import Footer from '@/components/sections/Footer';
-
-// import CustomCursor from '@/components/ui/CustomCursor';
-// import LoadingScreen from '@/components/ui/LoadingScreen';
-
 export default function HomePage() {
   return (
-    <>
-      {/* <CustomCursor /> */}
-      {/* <LoadingScreen /> */}
-
-      <main className="gradient-bg min-h-screen">
-        <Navbar />
-        <HeroSection />
-        <FeaturesSection />
-        <ScreenshotSection />
-        <ServerStatusSection />
-        <CommunitySection />
-        <Footer />
-      </main>
-    </>
+    <main style={{
+      background: "black",
+      color: "white",
+      height: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: "40px"
+    }}>
+      CLOZHI SMP TEST
+    </main>
   );
-}
+}git
